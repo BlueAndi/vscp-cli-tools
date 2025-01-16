c3e82fdf8c13d166da69aa2ec91bc356d683647b
BlueAndi <web@blue-andi.de>
2025-01-17 00:06:29 +0100

    Define VERSION and BUILDTYPE to get rid of editor showing an error, because both will be injected by the makefile.

--- programmer/main.c
+++ programmer/main.c
@@ -60,6 +60,16 @@ This module contains the main entry point.
     CONSTANTS
 *******************************************************************************/
 
+#ifndef VERSION
+/** Version */
+#define VERSION "x.x.x"
+#endif /* VERSION */
+
+#ifndef BUILDTYPE
+/** Build type */
+#define BUILDTYPE "debug"
+#endif /* BUILDTYPE */
+
 /** Program name */
 #define MAIN_PROG_NAME                  "VSCP L1 programmer"
 
